3b6ca3513592b2fb57f332670f67ed19ba7e498c
Allison Bohannon <akboh.github@gmail.com>
2022-11-26 18:35:13 -0500

    Create skeleton CRUD requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,26 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/books" do
+    books = Book.all
+    books.to_json
+  end
+
+  post "/books" do 
+    book = Book.create(
+      title: params[:title]
+    )
+  end
+
+  patch '/books/:id' do 
+    book = Book.find(params[:id])
+    book.to_json
+  end
+
+  delete '/books/:id' do
+    book = Book.find(params[:id])
+    book.destroy
+    book.to_json
   end
 
 end
