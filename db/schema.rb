6eeb899fa188f8870e2658880028216def660a1a
Allison Bohannon <akboh.github@gmail.com>
2022-11-26 18:21:52 -0500

    Create migration to create genre table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_26_231619) do
+ActiveRecord::Schema.define(version: 2022_11_26_231936) do
 
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
@@ -22,6 +22,10 @@ ActiveRecord::Schema.define(version: 2022_11_26_231619) do
     t.boolean "read_status"
     t.integer "author_id"
     t.integer "genre_id"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "genre"
   end
 
 end
