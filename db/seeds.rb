11ceb33616612999c3d1958ab342bd09fa47df4e
Allison Bohannon <akboh.github@gmail.com>
2022-12-03 23:29:49 -0500

    Reduce seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,20 +4,20 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 
-10.times do 
+3.times do 
     Author.create(
         first_name: Faker::Name.first_name, 
         last_name: Faker::Name.last_name
     )
 end
 
-10.times do 
+3.times do 
     Genre.create(
         genre: Faker::Book.genre
     )
 end
 
-50.times do
+10.times do
     Book.create(
         title: Faker::Book.title,
         read_status: 0, 
